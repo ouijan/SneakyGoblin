988b908d8dc706676b248d18f547b3e0a53a98f2
ouijan <tobyharris93@gmail.com>
2024-02-04 21:48:29 +1100

    chore: :construction: initial commit

---
+++ Source/SneakyGoblin/SneakyGoblin.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SneakyGoblin.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SneakyGoblin, "SneakyGoblin" );
